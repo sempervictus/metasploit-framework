4625e3289076575cea912ac5af67dee792610cc8
HD Moore <hd_moore@rapid7.com>
2014-06-24 13:23:51 -0500

    Finish deconflicting from master

--- lib/msf/core/payload/stager.rb
+++ lib/msf/core/payload/stager.rb
@@ -107,13 +107,19 @@ module Msf::Payload::Stager
   #
   # @return [String] The generated payload stage, as a string.
   def generate_stage
+    # XXX: This is nearly identical to Payload#internal_generate
+
     # Compile the stage as necessary
-    p = build(stage_payload, stage_assembly, stage_offsets, '-stg1')
+    if stage_assembly and !stage_assembly.empty?
+      raw = build(stage_assembly, stage_offsets)
+    else
+      raw = stage_payload.dup
+    end
 
     # Substitute variables in the stage
-    substitute_vars(p, stage_offsets) if (stage_offsets)
+    substitute_vars(raw, stage_offsets) if (stage_offsets)
 
-    return p
+    return raw
   end
 
   #
@@ -179,11 +185,6 @@ module Msf::Payload::Stager
   # @param (see Handler#create_session)
   # @return (see Handler#create_session)
   def handle_connection_stage(conn, opts={})
-    # Allow passing session transport information down from user level
-    # At present only used with standard meterpreters to switch between TLS and SSL
-    if datastore['TransportSSLVersion'] and not opts.has_key?(:transport_ssl_version)
-      opts.merge!({:transport_ssl_version => datastore['TransportSSLVersion'].intern})
-    end
     create_session(conn, opts)
   end
 
